7afe280e484bbebeb5857175b267fcc20e3f3077
fevin1414 <fevinbiju1414@gmail.com>
2023-09-11 11:13:31 -0500

    added eventid Page  from database

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,18 +1,14 @@
-import { Fragment } from 'react';
-import { useRouter } from 'next/router';
+import { Fragment } from "react";
 
-import { getEventById } from '../../dummy-data';
-import EventSummary from '../../components/event-detail/event-summary';
-import EventLogistics from '../../components/event-detail/event-logistics';
-import EventContent from '../../components/event-detail/event-content';
-import ErrorAlert from '../../components/ui/error-alert';
+import { getEventById,getAllEvents } from "../../helpers/api-util";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert";
 
-function EventDetailPage() {
-  const router = useRouter();
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
-
+function EventDetailPage(props) {
+  const event = props.selectedEvent;
   if (!event) {
     return (
       <ErrorAlert>
@@ -36,5 +32,26 @@ function EventDetailPage() {
     </Fragment>
   );
 }
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
 
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event
+    },
+    revalidate: 30
+  };
+}
+
+export async function getStaticPaths(){
+  const events=await getAllEvents();
+
+  const paths=events.map(event =>({params:{eventId: event.id}}));
+  return {
+    paths:paths,
+    fallback:false
+  };
+}
 export default EventDetailPage;
